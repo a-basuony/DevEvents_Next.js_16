1e7aaa1212b7d6fe72c9da4d9eb7637ecd549516
Ahmed Basuony <A.Basuony.social@gmail.com>
2025-11-30 03:59:34 +0200

    comply with v16

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -1,157 +1,18 @@
-import BookEvent from "@/components/BookEvent";
-import EventCard from "@/components/EventCard";
-import { IEvent } from "@/database";
-import { getSimilarEventsBySlug } from "@/lib/actions/event.actions";
-import Image from "next/image";
-import { notFound } from "next/navigation";
-
-const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
-
-const EventDetailItem = ({
-  icon,
-  alt,
-  label,
-}: {
-  icon: string;
-  alt: string;
-  label: string;
-}) => {
-  return (
-    <div className=" flex-row-gap-2 items-center">
-      <Image src={icon} alt={alt} width={17} height={17} />
-      <p>{label}</p>
-    </div>
-  );
-};
-
-const EventAgenda = ({ agendaItems }: { agendaItems: string[] }) => {
-  return (
-    <div className="agenda">
-      <h2>Agenda</h2>
-      <ul>
-        {agendaItems.map((item) => (
-          <li key={item}>{item}</li>
-        ))}
-      </ul>
-    </div>
-  );
-};
-
-const EventTags = ({ tags }: { tags: string[] }) => {
-  return (
-    <div className="flex flex-row gap-1.5 flex-wrap">
-      {tags.map((tag) => (
-        <div className="pill" key={tag}>
-          {tag}
-        </div>
-      ))}
-    </div>
-  );
-};
-
-const booking = 10;
+import EventDetails from "@/components/EventDetails";
+import { Suspense } from "react";
 
 const EventDetailsPage = async ({
   params,
 }: {
   params: Promise<{ slug: string }>;
 }) => {
-  const { slug } = await params;
-
-  const response = await fetch(`${BASE_URL}/api/events/${slug}`);
-  const {
-    event: {
-      description,
-      title,
-      image,
-      date,
-      location,
-      tags,
-      time,
-      mode,
-      audience,
-      agenda,
-      organizer,
-      overview,
-    },
-  } = await response.json();
-
-  if (!description) notFound();
-
-  const similarEvents = await getSimilarEventsBySlug(slug);
-
+  const slug = parmas.then((params) => params.slug);
   return (
-    <section id="event">
-      <div className="header">
-        <h1> Event Description</h1>
-        <p>{description}</p>
-      </div>
-      <div className="details">
-        {/* Left side - Event Content */}
-
-        <div className="content">
-          <Image
-            src={image}
-            alt="Event Banner"
-            className="banner"
-            width={800}
-            height={800}
-          />
-          <section className="flex-col-gap-2">
-            <h2>Overview</h2>
-            <p>{overview}</p>
-          </section>
-          <section className="flex-col-gap-2">
-            <h2>Event Details</h2>
-
-            <EventDetailItem
-              icon="/icons/calendar.svg"
-              alt="date"
-              label={date}
-            />
-            <EventDetailItem icon="/icons/clock.svg" alt="clock" label={time} />
-            <EventDetailItem icon="/icons/pin.svg" alt="pin" label={location} />
-            <EventDetailItem icon="/icons/mode.svg" alt="mode" label={mode} />
-            <EventDetailItem
-              icon="/icons/audience.svg"
-              alt="audience"
-              label={audience}
-            />
-          </section>
-          <EventAgenda agendaItems={agenda} />
-          <section className="flex-col-gap-2">
-            <h2>About TheOrganizer</h2>
-            <p>{organizer}</p>
-          </section>
-          <EventTags tags={tags} />
-        </div>
-
-        {/* Right side - Booking Form */}
-        <aside className="booking">
-          <div className="signup-card">
-            <h2>Book Your Spot</h2>
-            {booking > 0 ? (
-              <p className="text-sm">
-                Join {booking} people who have already booked
-              </p>
-            ) : (
-              <p className="text-sm">Be the first to book your spot!</p>
-            )}
-
-            <BookEvent />
-          </div>
-        </aside>
-      </div>
-      <div className="flex w-full flex-col gap-4 pt-20">
-        <h2>Similar Events</h2>
-        <div className="events">
-          {similarEvents.length > 0 &&
-            similarEvents.map((similarEvent: IEvent) => (
-              <EventCard key={similarEvent.title} {...similarEvent} />
-            ))}
-        </div>
-      </div>
-    </section>
+    <main>
+      <Suspense fallback={<div>Loading...</div>}>
+        <EventDetails params={slug} />
+      </Suspense>
+    </main>
   );
 };
 
